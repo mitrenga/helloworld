b83102f1076dc0cee172ae0d51128476af62efe6
Libor Mitrenga <libor@mitrengovi.cz>
2025-06-22 18:12:02 +0200

    Odstranění HelloWorldEntity

--- app/helloWorldModel.js
+++ app/helloWorldModel.js
@@ -1,9 +1,9 @@
 /**/
 const { AbstractModel } = await import('./svision/js/abstractModel.js?ver='+window.srcVersion);
-const { HelloWorldEntity } = await import('./helloWorldEntity.js?ver='+window.srcVersion);
+const { ZXTextEntity } = await import('./svision/js/platform/canvas2D/zxSpectrum/zxTextEntity.js?ver='+window.srcVersion);
 /*/
 import AbstractModel from './svision/js/abstractModel.js';
-import HelloWorldEntity from './helloWorldEntity.js';
+import ZXTextEntity from './svision/js/platform/canvas2D/zxSpectrum/zxTextEntity.js';
 /**/
 // begin code
 
@@ -19,7 +19,7 @@ export class HelloWorldModel extends AbstractModel {
 
     this.borderEntity.bkColor = this.app.platform.colorByName('green');
     this.desktopEntity.bkColor = this.app.platform.colorByName('white');
-    this.desktopEntity.addEntity(new HelloWorldEntity(this.desktopEntity, 0, 0, this.desktopWidth, 8, this.app.platform.colorByName('red')));
+    this.desktopEntity.addEntity(new ZXTextEntity(this.desktopEntity, 0, 0, this.desktopWidth, 8, 'Hello world!', this.app.platform.colorByName('red'), false, 0, false));
   } // init
 
 } // class HelloWorldModel
